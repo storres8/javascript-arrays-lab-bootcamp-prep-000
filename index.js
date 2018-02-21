3d4157800497e18134e9872b763240695d05e2b3
Steven Torres <stevetorres7562@gmail.com>
2018-02-21 18:22:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]; 
 
-function destructivelyAppendKitten (name) { 
+function destructivelyAppendKitten(name) { 
   kittens.push(name); 
   return kittens; 
 }
