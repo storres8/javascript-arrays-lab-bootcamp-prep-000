dec205695a1f6203d5534ff9bc8cb3097af62764
Steven Torres <stevetorres7562@gmail.com>
2018-02-21 18:26:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ function destructivelyAppendKitten(name) {
   return kittens; 
 }
 
-function destructivelyPrependKitten(name)
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
